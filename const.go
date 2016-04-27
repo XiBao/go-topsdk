3471162e1d624e528f57f857ddc50c723578e853
Syd Xu <thepeppersstudio@gmail.com>
2016-04-27 13:40:59 +0800

    change api gateway from http to https

--- const.go
+++ const.go
@@ -1,8 +1,8 @@
 package topsdk
 
 const (
-	GATEWAY_URL         = "http://gw.api.taobao.com/router/rest"
-	SANDBOX_GATEWAY_URL = "http://gw.api.tbsandbox.com/router/rest"
+	GATEWAY_URL         = "https://eco.taobao.com/router/rest"
+	SANDBOX_GATEWAY_URL = "https://gw.api.tbsandbox.com/router/rest"
 	JSON_FORMAT         = "json"
 	XML_FORMAT          = "xml"
 	SIGN_METHOD         = "md5"
